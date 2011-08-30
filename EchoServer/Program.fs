56059c43e0bce3952fed4f12988d26551dc12e4d
Gab_km <the25thcromosome@gmail.com>
2011-08-31 00:40:44 +0900

    関数内関数を全て外出しにした。

--- EchoServer/Program.fs
+++ EchoServer/Program.fs
@@ -4,6 +4,31 @@ open System.Net
 open System.Net.Sockets
 open System.Text
 
+let rec doLoop (ns : NetworkStream) (ms : MemoryStream) (ba: byte array) =
+    let resSize = ns.Read (ba, 0, ba.Length)
+    if resSize = 0 then
+        Console.WriteLine("Client has closed.")
+        Console.Read () |> ignore
+    else
+        ms.Write(ba, 0, resSize)
+        if ns.DataAvailable then
+            doLoop ns ms ba
+        else
+            let enc = Encoding.UTF8
+            Console.WriteLine(enc.GetString(ms.ToArray()))
+
+let recieveData (ns : NetworkStream) =
+    use mStream = new MemoryStream ()
+    let resBytes : byte array = Array.zeroCreate 256
+    doLoop ns mStream resBytes
+
+let rec myLoop (lsn : TcpListener) =
+    use tcp = lsn.AcceptTcpClient ()
+    Console.WriteLine("Client has connected.")
+    use nStream = tcp.GetStream ()
+    recieveData nStream
+    myLoop lsn
+ 
 [<EntryPoint>]
 let main _ =
     let ipAddr = Dns.GetHostEntry("localhost").AddressList.[0]
@@ -11,28 +36,6 @@ let main _ =
     try
         listener.Start()
         Console.WriteLine("Echo server starts.")
-        let rec myLoop (lsn : TcpListener) =
-            use tcp = lsn.AcceptTcpClient ()
-            Console.WriteLine("Client has connected.")
-            use nStream = tcp.GetStream ()
-            let recieveData (ns : NetworkStream) =
-                use mStream = new MemoryStream ()
-                let resBytes : byte array = Array.zeroCreate 256
-                let rec doLoop (ns : NetworkStream) (ms : MemoryStream) (ba: byte array) =
-                    let resSize = ns.Read (ba, 0, ba.Length)
-                    if resSize = 0 then
-                        Console.WriteLine("Client has closed.")
-                        Console.Read () |> ignore
-                    else
-                        ms.Write(ba, 0, resSize)
-                        if ns.DataAvailable then
-                            doLoop ns ms ba
-                        else
-                            let enc = Encoding.UTF8
-                            Console.WriteLine(enc.GetString(ms.ToArray()))
-                doLoop ns mStream resBytes
-            recieveData nStream
-            myLoop lsn
         myLoop listener
     finally
         listener.Stop ()
